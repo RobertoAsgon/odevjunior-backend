97214c80b9b49bd22bb929224283aaaa768afa83
Roberto Asgon <robertoasgon@gmail.com>
2021-05-22 19:08:52 -0300

    refactor: testando conexao heroku

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const db = require('../model/config');
 const route = express.Router();
 
 route
-  .get('/', (_req, res) => res.send(`Teste: ${db}`))
+  .get('/', (_req, res) => {
+    db.connect(function(err) {
+      if (err) throw err;
+      res.send("Connected!");
+    })
+  })
 
 module.exports = route;
