f3f641f1b623694b453fb8be65df41c5560c26a3
Roberto Asgon <robertoasgon@gmail.com>
2021-05-23 14:51:20 -0300

    feat: reconnect conexao db

--- model/connection.js
+++ model/connection.js
@@ -13,4 +13,30 @@ db.connect(error => {
   console.log("Successfully connected to the database.");
 });
 
+function handleDisconnect(conn) {
+  conn.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection({
+      host:"us-cdbr-east-03.cleardb.com",
+      user: "b09cb80e130998",
+      password: "defc81a4",
+      database: "heroku_7d6e59d42396b7f"
+    });
+
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
+
+handleDisconnect(db);
+
 module.exports = db;
